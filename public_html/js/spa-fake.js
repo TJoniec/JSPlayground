61bd819dd940bf2d736c17cfec3b778ff2eb7901
tony.joniec@gmail.com <tony.joniec@gmail.com>
2014-12-03 14:38:30 -0500

    Added the mock object for people to test with the person and persons object

---
+++ public_html/js/spa-fake.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * This module is used to create fake persons for testing prior to the implementation of the database
+ * this module is used by the -- fill in ----
+ */
+
+
+
+
