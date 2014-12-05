8830a194bf15e1738253c625ed5cba14a886f67b
tony.joniec@gmail.com <tony.joniec@gmail.com>
2014-12-05 11:21:58 -0500

    Placeholder for "TextPad Only" items

---
+++ public_html/js/js-mocks/mock_object_literals.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ *  
+ *   Just a  
+ */
+
+
+/* https://github.com/typicaljoe/taffydb */
+
+var products = TAFFY([{
+  "item":1,
+    "name":"Blue Ray Player",
+    "price":99.99
+}, {
+    "item":2,
+    name:"3D TV",
+    price:1799.99
+}]);
+
